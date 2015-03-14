9c1e7b1ab3abcc796e9b94d78333aaa22f74e1f7
Kevin Bell <kevin.a.bell@gmail.com>
2015-03-13 18:03:07 -0700

    More line continuations

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ aws cloudformation update-stack \
           --stack-name circle-cloudformation-4 \
           --use-previous-template \
           --parameters \
-            ParameterKey=ArtifactUrl,ParameterValue=$ARTIFACT_URL
-            ParameterKey=AMI,UsePreviousValue=true
-            ParameterKey=SecurityGroup,UsePreviousValue=true
-            ParameterKey=CircleToken,UsePreviousValue=true
-            ParameterKey=KeyName,UsePreviousValue=true
+            ParameterKey=ArtifactUrl,ParameterValue=$ARTIFACT_URL \
+            ParameterKey=AMI,UsePreviousValue=true \
+            ParameterKey=SecurityGroup,UsePreviousValue=true \
+            ParameterKey=CircleToken,UsePreviousValue=true \
+            ParameterKey=KeyName,UsePreviousValue=true \
