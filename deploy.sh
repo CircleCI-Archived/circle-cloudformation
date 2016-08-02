639e57e567ef85f1cb2f2ad109d9b40cce2b9ba0
jloper3 <jloper3@gmail.com>
2016-08-02 09:04:40 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,11 @@ set -exu
 ARTIFACT_ROOT=https://circle-artifacts.com/gh/circleci/circle-cloudformation
 ARTIFACT_FILE=$(find $CIRCLE_ARTIFACTS -name *-jar-with-dependencies.jar)
 ARTIFACT_URL=$ARTIFACT_ROOT/$CIRCLE_BUILD_NUM/artifacts/0$ARTIFACT_FILE
-
+STACK_NAME=circle-cloudformation-$CIRCLE_BUILD_NUM
+_
 aws cloudformation create-stack \
-          --stack-name circle-cloudformation-$CIRCLE_BUILD_NUM \
+          --stack-name $STACK_NAME \
           --template-body file://template.json \
+          
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
           
