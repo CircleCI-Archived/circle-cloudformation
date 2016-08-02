e887fd1282864a5e6fe763b1816005c0c3aeda3f
jloper3 <jloper3@gmail.com>
2016-08-02 09:07:41 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ ARTIFACT_ROOT=https://circle-artifacts.com/gh/circleci/circle-cloudformation
 ARTIFACT_FILE=$(find $CIRCLE_ARTIFACTS -name *-jar-with-dependencies.jar)
 ARTIFACT_URL=$ARTIFACT_ROOT/$CIRCLE_BUILD_NUM/artifacts/0$ARTIFACT_FILE
 STACK_NAME=circle-cloudformation-$CIRCLE_BUILD_NUM
-_
+
 aws cloudformation create-stack \
           --stack-name $STACK_NAME \
           --template-body file://template.json \
