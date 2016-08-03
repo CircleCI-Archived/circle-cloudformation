61a3171c2daa48785e00a57d042f5c9b389ea514
jloper3 <jloper3@gmail.com>
2016-08-03 13:18:34 -0400

    add waitOnCompletion

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,18 @@ ARTIFACT_FILE=$(find $CIRCLE_ARTIFACTS -name *-jar-with-dependencies.jar)
 ARTIFACT_URL=$ARTIFACT_ROOT/$CIRCLE_BUILD_NUM/artifacts/0$ARTIFACT_FILE
 STACK_NAME=circle-cloudformation-$CIRCLE_BUILD_NUM
 
+function waitOnCompletion() {
+    STATUS=IN_PROGRESS
+    while expr "$STATUS" : '^.*PROGRESS' > /dev/null ; do
+        sleep 10
+        STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[0].StackStatus')
+        echo $STATUS
+    done
+}
+
 aws cloudformation create-stack \
           --stack-name $STACK_NAME \
           --template-body file://template.json \
           
-aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+waitOnCompletion
           
